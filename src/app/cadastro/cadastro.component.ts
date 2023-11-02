62bde5af216a90e35e238e5fe69e4c0726452cec
João Vitor Carvalho <njvcarvalho@gmail.com>
2023-11-02 16:22:21 -0300

    ✨add ngForm e validators to cadastropage

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,5 +1,7 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ConsultaCepService } from '../service/consulta-cep.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -8,12 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private cepService : ConsultaCepService
+    ) { }
 
   ngOnInit(): void {
   }
 
-  cadastrar(){
-      console.log('Formulário enviado');
+  consultaCep(ev:any, form:NgForm){
+    const cep = ev.target.value;
+    if(cep !== ""){
+      this.cepService.getConsultaCep(cep).subscribe(resultado =>
+        {
+        console.log(resultado); 
+        this.populandoEndereco(resultado,form);
+      });
+    }
+  }
+
+  populandoEndereco(dados:any, form:NgForm){
+    form.form.patchValue({
+      endereco: dados.logradouro,
+      complemento: dados.complemento,
+      bairro: dados.bairro,
+      cidade:dados.localidade,
+      estado: dados.uf
+    })
+  }
+
+  cadastrar(form: NgForm){
+    // if(form.valid){
+    //    this.router.navigate(['./sucesso'])
+    // }else{
+    //   alert('Formulario Invalido')
+    // }
+      console.log(form.controls);
   }
 }
